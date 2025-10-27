da66dc8a3fde8744f80d315e3c347c1a35614eab
Dom <im.sekiel@gmail.com>
2025-10-27 09:44:18 -0700

    ADDED: Chaos in the Grid problem

---
+++ random/dp/chaosInTheGrid.js
@@ -0,0 +1,75 @@
+// You are given a grid represented as a nested array filled
+// with empty strings. Chaos, the puppy, is standing at the
+// top-left corner of the grid and can move either down or right
+// at any point in time. Determine the number of distinct paths
+// Chaos can take to reach a bowl of treats placed at the
+// bottom-right corner of the grid.
+
+// Define a function `chaosInTheGrid` that, given a nested
+// array, returns the number of unique paths that Chaos
+// can take to reach the bottom-right corner.
+
+// The grid will have at least 1 row and 1 column.
+
+// Example:
+
+// Given the following 2x3 grid:
+
+const grid = [
+  ["", "", ""],
+  ["", "", ""],
+];
+
+// There are three distinct path Chaos can take:
+// 1. Right -> Right -> Down
+// 2. Right -> Down -> Right
+// 3. Down -> Right -> Right
+
+function chaosInTheGrid(grid) {
+  for (let i = 0; i < grid.length; i += 1) {
+    grid[i][0] = 1;
+  }
+
+  grid[0].fill(1);
+  
+  for (let row = 1; row < grid.length; row += 1) {
+    let arr = grid[row];
+    for(let col = 1; col < arr.length; col += 1) {
+      grid[row][col] = grid[row - 1][col] + grid[row][col - 1];
+    }
+  }
+
+  return grid[grid.length - 1][grid[0].length - 1];
+}
+
+// Test cases
+
+const grid1 = [[""]];
+const grid2 = [
+  ["", ""],
+  ["", ""],
+];
+const grid3 = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
+const grid4 = [
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
+const grid5 = [
+  ["", "", "", "", "", ""],
+  ["", "", "", "", "", ""],
+  ["", "", "", "", "", ""],
+  ["", "", "", "", "", ""],
+  ["", "", "", "", "", ""],
+  ["", "", "", "", "", ""],
+];
+console.log(chaosInTheGrid(grid1) === 1);
+console.log(chaosInTheGrid(grid2) === 2);
+console.log(chaosInTheGrid(grid3) === 6);
+console.log(chaosInTheGrid(grid4) === 15);
+console.log(chaosInTheGrid(grid5) === 252);
+// All test cases should log true
