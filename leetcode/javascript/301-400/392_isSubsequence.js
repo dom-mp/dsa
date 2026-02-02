04ca806aa057a5ba1ac38420e669c344651f7e74
Dom <im.sekiel@gmail.com>
2026-02-02 13:05:08 -0800

    CHORE: Moved all current leetcode problems under javascript/ and introduced python/

--- leetcode/javascript/301-400/392_isSubsequence.js
+++ leetcode/javascript/301-400/392_isSubsequence.js
@@ -2,7 +2,6 @@ function isSubsequence(str1, str2) {
     let i = 0;
 
     for (let ii = 0; ii < str2.length; ii++) {
-        // console.log(str1[i])
         if (str1[i] === str2[ii]) {
             if (str1.length - 1 === i) return true;
             i++;
