3d6f8e46c97712c6bb0ac0424acb02648e296eb5
Dom <im.sekiel@gmail.com>
2026-03-06 16:49:15 -0800

    Added: Leetcode 238

---
+++ neetcode/neetcode150/products_of_array_except_self.py
@@ -0,0 +1,36 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefix = [1]
+        suffix = [1]
+
+        for num in nums:
+            last = prefix[len(prefix) - 1]
+            prefix.append(last * num)
+
+            if len(prefix) >= len(nums):
+                break
+        
+        for i in range(len(nums) - 1, -1, -1):
+            curr = nums[i]
+            peak = suffix[0]
+
+            suffix.insert(0, curr * peak)
+
+            if len(suffix) >= len(nums):
+                break;
+
+        results = []
+
+        for i in range(len(prefix)):
+            num1 = prefix[i]
+            num2 = suffix[i]
+
+            results.append(num1 * num2)
+
+        return results
+# [1, 2, 4, 6]
+
+# [1, 1, 2, 8]
+# [48, 24, 6, 1]
+
+# [48, 24, 12, 8]
